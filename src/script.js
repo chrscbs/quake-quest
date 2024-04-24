c4e06f3de1372e7b96fb40a9378ac63be5f7de84
Fritz <fritzdariel@gmail.com>
2024-04-24 13:39:37 +0800

    added draft template for faq page

--- src/script.js
+++ src/script.js
@@ -76,3 +76,13 @@ $(document).ready(function(){
       }]
   });
 });
+
+var myAccordion = new gianniAccordion({
+  elements: ".card article",
+          trigger: "[data-accordion-element-trigger]",
+        content: "[data-accordion-element-content]",
+});
+
+myAccordion.on("elementSelected", (data)=>{
+  console.log("elementOpened", data);
+});
